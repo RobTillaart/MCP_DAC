b804132db6f5f1a8fbcbb9721a55292c3dfbf8bb
rob tillaart <rob.tillaart@gmail.com>
2021-11-08 14:41:35 +0100

    update build-CI, badges, default parameters

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -39,15 +39,81 @@ unittest_teardown()
 
 unittest(test_constructor)
 {
-  fprintf(stderr, "VERSION: %s\n", MCP_DAC_LIB_VERSION);
+  fprintf(stderr, "VERSION: %s\n", (char *) MCP_DAC_LIB_VERSION);
   MCP4921 MCP;
 
   fprintf(stderr, "test default values\n");
   assertEqual(4095, MCP.maxValue());
   assertEqual(0, MCP.lastValue());
+  assertEqual(1, MCP.channels());
 
-  fprintf(stderr, "test start\n");
+}
 
+
+unittest(test_gain)
+{
+  fprintf(stderr, "VERSION: %s\n", (char *) MCP_DAC_LIB_VERSION);
+  MCP4921 MCP;
+
+  assertEqual(1, MCP.getGain());
+
+  MCP.setGain(2);
+  assertEqual(2, MCP.getGain());
+
+  MCP.setGain(0);
+  assertEqual(2, MCP.getGain());
+
+  MCP.setGain();
+  assertEqual(1, MCP.getGain());
+}
+
+
+unittest(test_SPI_speed)
+{
+  fprintf(stderr, "VERSION: %s\n", (char *) MCP_DAC_LIB_VERSION);
+  MCP4921 MCP;
+
+  MCP.setSPIspeed(1000000);
+  assertEqual(1000000, MCP.getSPIspeed());
+
+  MCP.setSPIspeed(2000000);
+  assertEqual(2000000, MCP.getSPIspeed());
+
+  MCP.setSPIspeed(3000000);
+  assertEqual(3000000, MCP.getSPIspeed());
+
+  MCP.setSPIspeed(4000000);
+  assertEqual(4000000, MCP.getSPIspeed());
+}
+
+
+unittest(test_active)
+{
+  fprintf(stderr, "VERSION: %s\n", (char *) MCP_DAC_LIB_VERSION);
+  MCP4921 MCP;
+
+  assertTrue(MCP.isActive());
+
+  MCP.shutDown(true);
+  assertFalse(MCP.isActive());
+
+  MCP.reset();
+  assertTrue(MCP.isActive());
+}
+
+
+unittest(test_buffered_mode)
+{
+  fprintf(stderr, "VERSION: %s\n", (char *) MCP_DAC_LIB_VERSION);
+  MCP4921 MCP;
+
+  assertFalse(MCP.getBufferedMode());
+
+  MCP.setBufferedMode(true);
+  assertTrue(MCP.getBufferedMode());
+
+  MCP.setBufferedMode(false);
+  assertFalse(MCP.getBufferedMode());
 }
 
 
